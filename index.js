b316cc661ac2d2e08ac3edbe166d3a4387134f13
Nicolas Perez <nbome12@outlook.com>
2024-09-05 19:54:28 -0500

    second commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 routerApi(app);
 
 
-app.get('/users', (req, res) => {
+/*app.get('/users', (req, res) => {
   const { limit, offset } = req.query;
   if(limit && offset){
     res.json({
@@ -19,16 +19,8 @@ app.get('/users', (req, res) => {
   }else{
     res.send("No hay limit ni offset");
   }
-})
+})*/
 
-
-app.get('/categorias/:categoriaId/productos/:productosId', (req, res) => {
-  const { categoriaId, productosId } = req.params;
-  res.json({
-    categoriaId,
-    productosId
-  })
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
